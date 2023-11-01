89b3188cd4dacb8aa8f9076b75203015a18ee5ce
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-11-01 21:04:20 +0700

    first commit

---
+++ lib/phone.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyPhone extends StatefulWidget {
+  const MyPhone({super.key});
+
+  @override
+  State<MyPhone> createState() => _MyPhoneState();
+}
+
+class _MyPhoneState extends State<MyPhone> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
